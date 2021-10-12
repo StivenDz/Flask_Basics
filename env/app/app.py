e53ce932146f6fba067f04d6e4220c74b45f9b04
StivenDz <stivendiazh@gmail.com>
2021-10-12 09:55:18 -0500

    ready

--- env/app/app.py
+++ env/app/app.py
@@ -8,6 +8,7 @@ def index():
     cursos= ['PHP' ,'Python','Java','Kotlin','Dart','JavaScript']
     numeros =[1,2,3,4,5,6]
     data={
+        
         'titulo':'Drums School',
         'Bienvenida':'¡Saludos!',
         'cursos':cursos,
