1bd2fc3f4d0ec0856a7383a4f41bc227e2016eb4
StivenDz <stivendiazh@gmail.com>
2021-10-17 09:27:02 -0500

    Cambios en el app.py y se agregaron dos vistas mas

--- env/app/app.py
+++ env/app/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__,template_folder="templates")
+
+@app.before_request
+def before_request():
+    print('Antes de la peticion...')
+
+@app.after_request
+def bafter_request(response):
+    print('Despues de la peticion...')
+    return response
 
 @app.route('/')
 def index():
@@ -8,8 +17,8 @@ def index():
     cursos= ['PHP' ,'Python','Java','Kotlin','Dart','JavaScript']
     numeros =[1,2,3,4,5,6]
     data={
-        
-        'titulo':'Drums School',
+
+        'titulo':'Fundamentos Flask',
         'Bienvenida':'¡Saludos!',
         'cursos':cursos,
         'numeros':numeros,
@@ -19,15 +28,31 @@ def index():
     }
     return render_template('index.html',data=data)
 
-
-@app.route('/contacto/<nombre>')
-def contacto(nombre):
+# url personalizada, especialmente para usuarios, entre otras cosas, url dinamica
+@app.route('/contacto/<nombre>/<int:edad>')
+def contacto(nombre,edad):
     data={
         'titulo': 'contacto',
-        'nombre':nombre
+        'nombre':nombre,
+        'edad':edad
     }
     return render_template('contacto.html',data=data)
 
+def query_string():
+    print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
+    return "ok"
+
+def pagina_no_encontrada(error):
+    data={
+        'titulo':'Página no Encontrada'
+    }
+    #return render_template('404.html',data=data),404
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    app.add_url_rule('/query_string',view_func=query_string)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True, port=9999)
